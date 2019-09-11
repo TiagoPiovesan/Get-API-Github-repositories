db0d4d742a6941497ed8d4d1a637eb1810209a2b
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2019-09-11 17:26:41 -0300

    add axios and style in front-page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Title } from './style';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-export default function Main() {
-  return <Title>Main</Title>;
+import { Container, Form, SubmitButton, List } from './style';
+
+import api from '../../services/api';
+
+export default class Main extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, repositories, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {/* if ternário, se loading = true, spinner, else, plus */}
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="">Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
